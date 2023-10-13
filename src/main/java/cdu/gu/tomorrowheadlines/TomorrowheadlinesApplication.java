70e719d1819f85f5d0b53e3b401b1630b8eea77a
onelibero <1792208603@qq.com>
2023-10-13 20:50:46 +0800

    初始化项目

---
+++ src/main/java/cdu/gu/tomorrowheadlines/TomorrowheadlinesApplication.java
@@ -0,0 +1,13 @@
+package cdu.gu.tomorrowheadlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomorrowheadlinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomorrowheadlinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cdu/gu/tomorrowheadlines/TomorrowheadlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package cdu.gu.tomorrowheadlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowheadlinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
