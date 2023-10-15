a235bfaca6a8965dfbb2d3806257d6d42da23674
onelibero <1792208603@qq.com>
2023-10-15 13:40:51 +0800

    “提交了调用模块”

---
+++ feign-api/src/main/java/cdu/gu/feignapi/FeignApiApplication.java
@@ -0,0 +1,13 @@
+package cdu.gu.feignapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApiApplication.class, args);
+	}
+
+}
