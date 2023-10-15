f16bd956e8b5196477d6fce1adc5b10611d87300
onelibero <1792208603@qq.com>
2023-10-15 13:43:30 +0800

    修改了部分

---
+++ model/src/test/java/cdu/gu/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package cdu.gu.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
